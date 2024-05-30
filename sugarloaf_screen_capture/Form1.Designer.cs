0e76a0d5c5d310dee9c4a16ef46a34129bf4ba80
AnochaPay <Anocha_ping@hotmail.com>
2024-05-30 15:59:18 +0700

    add

---
+++ sugarloaf_screen_capture/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace sugarloaf_screen_capture {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.display_text_pass = new System.Windows.Forms.ToolStripMenuItem();
+            this.textfalseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.display_text_false = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ROI_text = new System.Windows.Forms.ToolStripMenuItem();
+            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ContextMenuStrip = this.contextMenuStrip1;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "display";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.textToolStripMenuItem,
+            this.textfalseToolStripMenuItem,
+            this.runToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(123, 70);
+            // 
+            // textToolStripMenuItem
+            // 
+            this.textToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.display_text_pass});
+            this.textToolStripMenuItem.Name = "textToolStripMenuItem";
+            this.textToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.textToolStripMenuItem.Text = "text_pass";
+            // 
+            // display_text_pass
+            // 
+            this.display_text_pass.Name = "display_text_pass";
+            this.display_text_pass.Size = new System.Drawing.Size(89, 22);
+            this.display_text_pass.Text = "xxx";
+            this.display_text_pass.Click += new System.EventHandler(this.display_text_Click);
+            // 
+            // textfalseToolStripMenuItem
+            // 
+            this.textfalseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.display_text_false});
+            this.textfalseToolStripMenuItem.Name = "textfalseToolStripMenuItem";
+            this.textfalseToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.textfalseToolStripMenuItem.Text = "text_false";
+            // 
+            // display_text_false
+            // 
+            this.display_text_false.Name = "display_text_false";
+            this.display_text_false.Size = new System.Drawing.Size(89, 22);
+            this.display_text_false.Text = "xxx";
+            this.display_text_false.Click += new System.EventHandler(this.display_text_false_Click);
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.runToolStripMenuItem.Text = "run";
+            this.runToolStripMenuItem.Click += new System.EventHandler(this.runToolStripMenuItem_Click);
+            // 
+            // button2
+            // 
+            this.button2.ContextMenuStrip = this.contextMenuStrip2;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button2.Location = new System.Drawing.Point(196, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 59);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ROI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.textToolStripMenuItem1,
+            this.testToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(94, 48);
+            // 
+            // textToolStripMenuItem1
+            // 
+            this.textToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ROI_text});
+            this.textToolStripMenuItem1.Name = "textToolStripMenuItem1";
+            this.textToolStripMenuItem1.Size = new System.Drawing.Size(93, 22);
+            this.textToolStripMenuItem1.Text = "text";
+            // 
+            // ROI_text
+            // 
+            this.ROI_text.Name = "ROI_text";
+            this.ROI_text.Size = new System.Drawing.Size(89, 22);
+            this.ROI_text.Text = "xxx";
+            this.ROI_text.Click += new System.EventHandler(this.ROI_text_Click);
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.testToolStripMenuItem.Text = "test";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 149);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(363, 145);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 83);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem textToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem display_text_pass;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem textToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ROI_text;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem textfalseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem display_text_false;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ sugarloaf_screen_capture/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using IronOcr;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace sugarloaf_screen_capture {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            try { display_text_pass.Text = File.ReadAllText("../../config/sugarloaf_screen_display_text_pass.txt"); } catch { }
+            try { display_text_false.Text = File.ReadAllText("../../config/sugarloaf_screen_display_text_false.txt"); } catch { }
+            try { ROI_text.Text = File.ReadAllText("../../config/sugarloaf_screen_roi_text.txt"); } catch { }
+        }
+
+        Form fff;
+        Graphics g;
+        Pen myPen;
+        bool flag_pen = false;
+        Point StartPoint, StopPoint;
+        Point roi_point;
+        Size roi_size;
+        private void button1_Click(object sender, EventArgs e) {
+            this.Hide();
+            System.Threading.Thread.Sleep(100);
+            fff = new Form();
+            fff.Size = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            fff.Location = new Point(0, 0);
+            fff.ControlBox = false;
+            fff.TransparencyKey = Color.Red;
+            fff.BackColor = Color.Red;
+            //fff.Opacity = 0.01;
+            fff.Cursor = Cursors.Cross;
+            g = fff.CreateGraphics();
+            myPen = new Pen(Color.Blue);
+            myPen.Width = 2;
+            fff.MouseDown += new MouseEventHandler(MouseDown_display);
+            fff.MouseMove += new MouseEventHandler(mouseMove_display);
+            fff.MouseUp += new MouseEventHandler(mouseUp_display);
+            fff.Show();
+        }
+        private void button2_Click(object sender, EventArgs e) {
+            this.Hide();
+            System.Threading.Thread.Sleep(100);
+            fff = new Form();
+            fff.Size = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            fff.Location = new Point(0, 0);
+            fff.ControlBox = false;
+            fff.TransparencyKey = Color.Red;
+            fff.BackColor = Color.Red;
+            //fff.Opacity = 0.01;
+            fff.Cursor = Cursors.Cross;
+            g = fff.CreateGraphics();
+            myPen = new Pen(Color.Blue);
+            myPen.Width = 2;
+            fff.MouseDown += new MouseEventHandler(MouseDown_roi);
+            fff.MouseMove += new MouseEventHandler(mouseMove_roi);
+            fff.MouseUp += new MouseEventHandler(mouseUp_roi);
+            fff.Show();
+        }
+        private void mouseMove_display(object sender, MouseEventArgs e) {
+            if (flag_pen != true) return;
+            g.Clear(Color.Red);
+            int pointMin_x = ((StartPoint.X + Control.MousePosition.X) - Convert.ToInt32(Math.Sqrt(Math.Pow(Convert.ToDouble(StartPoint.X - Control.MousePosition.X), 2)))) / 2;
+            int pointMin_y = ((StartPoint.Y + Control.MousePosition.Y) - Convert.ToInt32(Math.Sqrt(Math.Pow(Convert.ToDouble(StartPoint.Y - Control.MousePosition.Y), 2)))) / 2;
+            g.DrawRectangle(myPen, pointMin_x - 10, pointMin_y - 10, Convert.ToInt32(Math.Sqrt(Math.Pow(Convert.ToDouble(Control.MousePosition.X - StartPoint.X), 2))) + 4, Convert.ToInt32(Math.Sqrt(Math.Pow(Convert.ToDouble(Control.MousePosition.Y - StartPoint.Y), 2)) + 4));
+        }
+        private void MouseDown_display(object sender, MouseEventArgs e) {
+            StartPoint = new Point(Control.MousePosition.X, Control.MousePosition.Y);
+            File.WriteAllText("../../config/sugarloaf_screen_point_start_display_x.txt", Control.MousePosition.X.ToString());
+            File.WriteAllText("../../config/sugarloaf_screen_point_start_display_y.txt", Control.MousePosition.Y.ToString());
+            flag_pen = true;
+        }
+        private void mouseUp_display(object sender, MouseEventArgs e) {
+            File.WriteAllText("../../config/sugarloaf_screen_point_stop_display_x.txt", Control.MousePosition.X.ToString());
+            File.WriteAllText("../../config/sugarloaf_screen_point_stop_display_y.txt", Control.MousePosition.Y.ToString());
+            flag_pen = false;
+            fff.Close();
+            System.Threading.Thread.Sleep(100);
+            this.Show();
+        }
+        private void mouseMove_roi(object sender, MouseEventArgs e) {
+            if (flag_pen != true) return;
+            g.Clear(Color.Red);
+            int pointMin_x = ((StartPoint.X + Control.MousePosition.X) - Convert.ToInt32(Math.Sqrt(Math.Pow(Convert.ToDouble(StartPoint.X - Control.MousePosition.X), 2)))) / 2;
+            int pointMin_y = ((StartPoint.Y + Control.MousePosition.Y) - Convert.ToInt32(Math.Sqrt(Math.Pow(Convert.ToDouble(StartPoint.Y - Control.MousePosition.Y), 2)))) / 2;
+            g.DrawRectangle(myPen, pointMin_x - 10, pointMin_y - 10, Convert.ToInt32(Math.Sqrt(Math.Pow(Convert.ToDouble(Control.MousePosition.X - StartPoint.X), 2))) + 4, Convert.ToInt32(Math.Sqrt(Math.Pow(Convert.ToDouble(Control.MousePosition.Y - StartPoint.Y), 2)) + 4));
+        }
+        private void MouseDown_roi(object sender, MouseEventArgs e) {
+            StartPoint = new Point(Control.MousePosition.X, Control.MousePosition.Y);
+            File.WriteAllText("../../config/sugarloaf_screen_point_start_roi_x.txt", Control.MousePosition.X.ToString());
+            File.WriteAllText("../../config/sugarloaf_screen_point_start_roi_y.txt", Control.MousePosition.Y.ToString());
+            flag_pen = true;
+        }
+        private void mouseUp_roi(object sender, MouseEventArgs e) {
+            Image img;
+            File.WriteAllText("../../config/sugarloaf_screen_point_stop_roi_x.txt", Control.MousePosition.X.ToString());
+            File.WriteAllText("../../config/sugarloaf_screen_point_stop_roi_y.txt", Control.MousePosition.Y.ToString());
+            roi_size = new Size(Convert.ToInt32(Math.Sqrt(Math.Pow(StartPoint.X - Control.MousePosition.X, 2))), Convert.ToInt32(Math.Sqrt(Math.Pow(StartPoint.Y - Control.MousePosition.Y, 2))));
+            using (Bitmap bitmap = new Bitmap(roi_size.Width, roi_size.Height)) {
+                using (Graphics g = Graphics.FromImage(bitmap)) {
+                    g.CopyFromScreen(StartPoint, Point.Empty, bitmap.Size);
+                }
+                img = new Bitmap((Image)bitmap);
+            }
+            img.Save("../../config/image_ref.png", ImageFormat.Png);
+            flag_pen = false;
+            fff.Close();
+            System.Threading.Thread.Sleep(100);
+            this.Show();
+        }
+
+        Form display = new Form();
+        Label label = new Label();
+        private void runToolStripMenuItem_Click(object sender, EventArgs e) {
+            double a = 0, b = 0, x = 0, y = 0;
+            try { a = Convert.ToInt32(File.ReadAllText("../../config/sugarloaf_screen_point_start_display_x.txt")); } catch { }
+            try { x = Convert.ToInt32(File.ReadAllText("../../config/sugarloaf_screen_point_start_display_y.txt")); } catch { }
+            try { b = Convert.ToInt32(File.ReadAllText("../../config/sugarloaf_screen_point_stop_display_x.txt")); } catch { }
+            try { y = Convert.ToInt32(File.ReadAllText("../../config/sugarloaf_screen_point_stop_display_y.txt")); } catch { }
+            display.Show();
+            display.FormBorderStyle = FormBorderStyle.None;
+            display.Location = new Point(Convert.ToInt32(((a + b) - Math.Sqrt(Math.Pow((a - b), 2))) / 2), Convert.ToInt32(((x + y) - Math.Sqrt(Math.Pow((x - y), 2))) / 2));
+            display.Size = new Size(Convert.ToInt32(Math.Sqrt(Math.Pow(a - b, 2))), Convert.ToInt32(Math.Sqrt(Math.Pow(x - y, 2))));
+            FontFamily fontFamily = new FontFamily("Arial");
+            label.Font = new Font(fontFamily, 40, FontStyle.Bold, GraphicsUnit.Pixel);
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            label.Location = new Point(0, 0);
+            label.Size = new Size(display.Size.Width, display.Size.Height);
+            label.Text = display_text_pass.Text;
+            display.Controls.Add(label);
+
+            try { a = Convert.ToInt32(File.ReadAllText("../../config/sugarloaf_screen_point_start_roi_x.txt")); } catch { }
+            try { x = Convert.ToInt32(File.ReadAllText("../../config/sugarloaf_screen_point_start_roi_y.txt")); } catch { }
+            try { b = Convert.ToInt32(File.ReadAllText("../../config/sugarloaf_screen_point_stop_roi_x.txt")); } catch { }
+            try { y = Convert.ToInt32(File.ReadAllText("../../config/sugarloaf_screen_point_stop_roi_y.txt")); } catch { }
+            roi_point.X = Convert.ToInt32(((a + b) - Math.Sqrt(Math.Pow((a - b), 2))) / 2);
+            roi_point.Y = Convert.ToInt32(((x + y) - Math.Sqrt(Math.Pow((x - y), 2))) / 2);
+            roi_size = new Size(Convert.ToInt32(Math.Sqrt(Math.Pow(a - b, 2))), Convert.ToInt32(Math.Sqrt(Math.Pow(x - y, 2))));
+
+            image_sup = new Bitmap(Image.FromFile("../../config/image_ref.png"));
+
+            //Application.Idle += loop;
+            backgroundWorker1.RunWorkerAsync();
+        }
+        private void display_text_Click(object sender, EventArgs e) {
+            while (true) {
+                string input = Microsoft.VisualBasic.Interaction.InputBox("_ใส่ข้อความ", "text", display_text_pass.Text, 500, 300);
+                if (input == "") return;
+                try {
+                    File.WriteAllText("../../config/sugarloaf_screen_display_text_pass.txt", input);
+                    try { display_text_pass.Text = File.ReadAllText("../../config/sugarloaf_screen_display_text_pass.txt"); } catch { }
+                } catch (Exception) {
+                    MessageBox.Show("not format");
+                    continue;
+                }
+                break;
+            }
+        }
+        private void display_text_false_Click(object sender, EventArgs e) {
+            while (true) {
+                string input = Microsoft.VisualBasic.Interaction.InputBox("_ใส่ข้อความ", "text", display_text_false.Text, 500, 300);
+                if (input == "") return;
+                try {
+                    File.WriteAllText("../../config/sugarloaf_screen_display_text_false.txt", input);
+                    try { display_text_false.Text = File.ReadAllText("../../config/sugarloaf_screen_display_text_false.txt"); } catch { }
+                } catch (Exception) {
+                    MessageBox.Show("not format");
+                    continue;
+                }
+                break;
+            }
+        }
+
+        private void ROI_text_Click(object sender, EventArgs e) {
+            while (true) {
+                string input = Microsoft.VisualBasic.Interaction.InputBox("_ใส่ข้อความ", "text", ROI_text.Text, 500, 300);
+                if (input == "") return;
+                try {
+                    File.WriteAllText("../../config/sugarloaf_screen_roi_text.txt", input);
+                    try { ROI_text.Text = File.ReadAllText("../../config/sugarloaf_screen_roi_text.txt"); } catch { }
+                } catch (Exception) {
+                    MessageBox.Show("not format");
+                    continue;
+                }
+                break;
+            }
+        }
+
+        Bitmap image;
+        Bitmap image_sup;
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e) {
+            while (true) {
+                using (Bitmap bitmap = new Bitmap(roi_size.Width, roi_size.Height)) {
+                    using (Graphics g = Graphics.FromImage(bitmap)) {
+                        g.CopyFromScreen(roi_point, Point.Empty, bitmap.Size);
+                    }
+                    image = new Bitmap((Image)bitmap);
+                }
+                pictureBox1.Image = image;
+                Color img1, img2;
+                bool flag_img = true;
+                for (int i = 0; i < image.Width; i += 1) {
+                    for (int j = 0; j < image.Height; j += 1) {
+                        img1 = image_sup.GetPixel(i, j);
+                        img2 = image.GetPixel(i, j);
+                        if (img1 != img2) {
+                            i = image.Width;
+                            flag_img = false;
+                            break;
+                        }
+                    }
+                }
+                if (flag_img) {
+                    backgroundWorker1.ReportProgress(0);
+                } else {
+                    backgroundWorker1.ReportProgress(1);
+                }
+            }
+        }
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e) {
+            if (e.ProgressPercentage == 0) {
+                display.BackColor = Color.Lime;
+                label.Text = display_text_pass.Text;
+            } else if (e.ProgressPercentage == 1){
+                display.BackColor = Color.Red;
+                label.Text = display_text_false.Text;
+            }
+        }
+
+        private void loop(object sender, EventArgs e) {
+            using (Bitmap bitmap = new Bitmap(roi_size.Width, roi_size.Height)) {
+                using (Graphics g = Graphics.FromImage(bitmap)) {
+                    g.CopyFromScreen(roi_point, Point.Empty, bitmap.Size);
+                }
+                image = new Bitmap((Image)bitmap);
+            }
+            pictureBox1.Image = image;
+            Color img1, img2;
+            bool flag_img = true;
+            for (int i = 0; i < image.Width; i += 1) {
+                for (int j = 0; j < image.Height; j += 1) {
+                    img1 = image_sup.GetPixel(i, j);
+                    img2 = image.GetPixel(i, j);
+                    if (img1 != img2) {
+                        i = image.Width;
+                        flag_img = false;
+                        break;
+                    }
+                }
+            }
+            if (flag_img) {
+                display.BackColor = Color.Lime;
+                label.Text = display_text_pass.Text;
+            } else {
+                display.BackColor = Color.Red;
+                label.Text = display_text_false.Text;
+            }
+            //AdvancedOcr Ocr = new AdvancedOcr() {
+            //    CleanBackgroundNoise = true,
+            //    EnhanceContrast = true,
+            //    EnhanceResolution = true,
+            //    Language = IronOcr.Languages.English.OcrLanguagePack,
+            //    Strategy = IronOcr.AdvancedOcr.OcrStrategy.Advanced,
+            //    ColorSpace = AdvancedOcr.OcrColorSpace.GrayScale,
+            //    DetectWhiteTextOnDarkBackgrounds = false,
+            //    RotateAndStraighten = true,
+            //    ReadBarCodes = false,
+            //    ColorDepth = 4
+            //};
+            //OcrResult Results = Ocr.Read(image);
+            //if (Results.ToString().Contains(ROI_text.Text)) {
+            //    display.BackColor = Color.Lime;
+            //    label.Text = display_text_pass.Text;
+            //} else {
+            //    display.BackColor = Color.Red;
+            //    label.Text = display_text_false.Text;
+            //}
+        }
+    }
+}
